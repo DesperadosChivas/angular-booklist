56593eb03bd64368f2fc4c9f2f0e81ced7f05f90
DesperadosChivas <ducquang.chu@gmail.com>
2019-09-30 23:06:50 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  constructor(){
+    var firebaseConfig = {
+    apiKey: "AIzaSyD7y8sM9_erV4BtMuB5hXbuJIunVHZ8lq8",
+    authDomain: "booklist-e1531.firebaseapp.com",
+    databaseURL: "https://booklist-e1531.firebaseio.com",
+    projectId: "booklist-e1531",
+    storageBucket: "",
+    messagingSenderId: "731307840724",
+    appId: "1:731307840724:web:e7024050dd9d7775b3d741",
+    measurementId: "G-VWBP94SS85"
+  };
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+  }
 }
